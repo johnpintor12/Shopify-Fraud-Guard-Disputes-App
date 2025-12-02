3382c452963398c71b60e39bdc91911a4dc0266c
johnpintor12 <johnjose.pintor12@gmail.com>
2025-12-02 14:02:25 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
-
   return {
     plugins: [react()],
     define: {
-      // This securely maps the system environment variable to the browser code
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      'process.env.API_KEY': JSON.stringify(env.API_KEY || '')
     }
   };
 });
