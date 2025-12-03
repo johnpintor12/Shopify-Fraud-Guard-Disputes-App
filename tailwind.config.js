6fbea910acd46b4bfba92bc019c283c61f1581de
johnpintor12 <johnjose.pintor12@gmail.com>
2025-12-03 15:12:46 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", /* Standard Vite structure */
+    "./**/*.{js,ts,jsx,tsx}", /* Standard Vite structure */
     "./*.{js,ts,jsx,tsx}",        /* ALSO scan root files just in case */
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
